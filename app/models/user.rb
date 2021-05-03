8c4b9e5730f6fa76eff70e2c331ada59d39b6bd6
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 19:43:06 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
+  validates_length_of :password, :minimum => 8
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
 
 end
