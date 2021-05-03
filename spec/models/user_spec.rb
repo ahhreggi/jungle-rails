cd090f35d1e9a2818ea30a3e5414c5f3ee2d59c3
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 19:33:40 -0400

    Add unique email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,50 +4,69 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     it "creates a new user given a first & last name, email, password, and valid password confirmation" do
-      user = User.new(
+      @user = User.new(
         first_name: "Reggi",
         last_name: "Sirilan",
         email: "rs@rs.ca",
         password: "password",
         password_confirmation: "password"
       )
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it "fails to create a new user if password is not provided" do
-      user = User.new(password: nil)
-      expect(user).to be_invalid
-      expect(user.errors[:password]).to include("cannot be blank")
-
-      user.password = "password"
-      user.valid?
-      expect(user.errors[password]).not_to include("cannot be blank")
+      @user = User.new(
+        first_name: "Reggi",
+        last_name: "Sirilan",
+        email: "rs@rs.ca",
+        password: nil,
+        password_confirmation: "password"
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages_for(:password)).to include "Password cannot be blank"
     end
 
     it "fails to create a new user if password_confirmation is not provided" do
-      user = User.new(password_confirmation: nil)
-      expect(user).to be_invalid
-      expect(user.errors[:password_confirmation]).to include("cannot be blank")
-
-      user.password_confirmation = "password"
-      user.valid?
-      expect(user.errors[password_confirmation]).not_to include("cannot be blank")
+      @user = User.new(
+        first_name: "Reggi",
+        last_name: "Sirilan",
+        email: "rs@rs.ca",
+        password: "password",
+        password_confirmation: nil
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages_for(:password_confirmation)).to include "Password confirmation cannot be blank"
     end
 
     it "fails to create a new user if password and password_confirmation do not match" do
-      user = User.new(
+      @user = User.new(
         first_name: "Reggi",
         last_name: "Sirilan",
         email: "rs@rs.ca",
         password: "password",
         password_confirmation: "wrongpassword"
       )
-      expect(user).to be_invalid
-      expect(user.errors[:password_confirmation]).to include("must match password")
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages_for(:password_confirmation)).to include "Password and confirmation must match"
+    end
 
-      user.password_confirmation = "password"
-      user.valid?
-      expect(user.errors[:password_confirmation]).not_to include("must match password")
+    it "fails to create a new user if the given email is already taken" do
+      @user1 = User.new(
+        first_name: "Reggi",
+        last_name: "Sirilan",
+        email: "rs@rs.ca",
+        password: "password",
+        password_confirmation: "password"
+      )
+      @user2 = User.new(
+        first_name: "Reggi2",
+        last_name: "Sirilan2",
+        email: "rs@rs.ca",
+        password: "password2",
+        password_confirmation: "password2"
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages_for(:email)).to include("Email has already been taken")
     end
 
   end
