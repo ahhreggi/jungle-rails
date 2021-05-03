01ff141910ac75fb8a07440a3c1850d5326e4db6
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 19:04:42 -0400

    Add tests to product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+
+    it "saves a product given a name, price, quantity, and category" do
+      @category = Category.create!(name: "Testing")
+      @product = Product.new(name: "Pandora's Box", price: 400, quantity: 3, category: @category)
+      expect(@product.save!).to eq true
+    end
+
+    it "fails to save a product if a name is not specified" do
+      @category = Category.create!(name: "Testing")
+      @product = Product.new(name: nil, price: 400, quantity: 3, category: @category)
+      expect(@product.errors.full_messages_for(:name)).to include "Name cannot be nil"
+    end
+
+    it "fails to save a product if a price is not specified" do
+      @category = Category.create!(name: "Testing")
+      @product = Product.new(name: "Pandora's Box", price: nil, quantity: 3, category: @category)
+      expect(@product.errors.full_messages_for(:price)).to include "Price cannot be nil"
+    end
+
+    it "fails to save a product if a quantity is not specified" do
+      @category = Category.create!(name: "Testing")
+      @product = Product.new(name: "Pandora's Box", price: 400, quantity: nil, category: @category)
+      expect(@product.errors.full_messages_for(:quantity)).to include "Quantity cannot be nil"
+    end
+
+    it "fails to save a product if a category is not specified" do
+      @category = Category.create!(name: "Testing")
+      @product = Product.new(name: "Pandora's Box", price: 400, quantity: 3, category: nil)
+      expect(@product.errors.full_messages_for(:category)).to include "Category cannot be nil"
+    end
+
   end
 end
