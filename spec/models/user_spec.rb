d6aa64d8db9c85c97721ac2dec1dcae573353641
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 19:24:06 -0400

    Add password confirmation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    it "fails to create a new user if a password is not provided" do
+    it "fails to create a new user if password is not provided" do
       user = User.new(password: nil)
       expect(user).to be_invalid
       expect(user.errors[:password]).to include("cannot be blank")
@@ -22,6 +22,32 @@ RSpec.describe User, type: :model do
       user.password = "password"
       user.valid?
       expect(user.errors[password]).not_to include("cannot be blank")
+    end
+
+    it "fails to create a new user if password_confirmation is not provided" do
+      user = User.new(password_confirmation: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:password_confirmation]).to include("cannot be blank")
+
+      user.password_confirmation = "password"
+      user.valid?
+      expect(user.errors[password_confirmation]).not_to include("cannot be blank")
+    end
+
+    it "fails to create a new user if password and password_confirmation do not match" do
+      user = User.new(
+        first_name: "Reggi",
+        last_name: "Sirilan",
+        email: "rs@rs.ca",
+        password: "password",
+        password_confirmation: "wrongpassword"
+      )
+      expect(user).to be_invalid
+      expect(user.errors[:password_confirmation]).to include("must match password")
+
+      user.password_confirmation = "password"
+      user.valid?
+      expect(user.errors[:password_confirmation]).not_to include("must match password")
     end
 
   end
