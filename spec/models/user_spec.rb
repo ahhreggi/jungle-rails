2c14e0b06440368c28bda3301ee3d9ff6840ae0f
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 19:15:59 -0400

    Add valid test to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "creates a new user given a first & last name, email, password, and valid password confirmation" do
+      user = User.new(
+        first_name: "Reggi",
+        last_name: "Sirilan",
+        email: "rs@rs.ca",
+        password: "password",
+        password_confirmation: "password"
+      )
+      expect(user).to be_valid
+    end
+
+  end
 end
