192ffa009f5036b1539a23621b4b30b34b2c41a1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 20:23:58 -0400

    Update user schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    user_params[:email] = user_params[:email].downcase.strip
+    @user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -18,7 +19,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :name,
+      :first_name,
+      :last_name,
       :email,
       :password,
       :password_confirmation
