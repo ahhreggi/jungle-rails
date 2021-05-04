74ec5b4f8648f23d2499b7957b0b63f9483acbd4
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-03 21:25:29 -0400

    Ensure passing validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,28 +13,28 @@ RSpec.describe Product, type: :model do
       @category = Category.create!(name: "Testing")
       @product = Product.new(name: nil, price: 400, quantity: 3, category: @category)
       @product.save
-      expect(@product.errors.full_messages_for(:name)).to include "Name cannot be nil"
+      expect(@product.errors.full_messages_for(:name)).to include "Name can't be blank"
     end
 
     it "fails to save a product if a price is not specified" do
       @category = Category.create!(name: "Testing")
       @product = Product.new(name: "Pandora's Box", price: nil, quantity: 3, category: @category)
       @product.save
-      expect(@product.errors.full_messages_for(:price)).to include "Price cannot be nil"
+      expect(@product.errors.full_messages_for(:price)).to include "Price can't be blank"
     end
 
     it "fails to save a product if a quantity is not specified" do
       @category = Category.create!(name: "Testing")
       @product = Product.new(name: "Pandora's Box", price: 400, quantity: nil, category: @category)
       @product.save
-      expect(@product.errors.full_messages_for(:quantity)).to include "Quantity cannot be nil"
+      expect(@product.errors.full_messages_for(:quantity)).to include "Quantity can't be blank"
     end
 
     it "fails to save a product if a category is not specified" do
       @category = Category.create!(name: "Testing")
       @product = Product.new(name: "Pandora's Box", price: 400, quantity: 3, category: nil)
       @product.save
-      expect(@product.errors.full_messages_for(:category)).to include "Category cannot be nil"
+      expect(@product.errors.full_messages_for(:category)).to include "Category can't be blank"
     end
 
   end
